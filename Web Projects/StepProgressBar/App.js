00807919f6000f996eca1e1c2f5b4f469a625e2f
Sabare1 <sabaregirivasan@gmail.com>
2025-07-08 13:08:46 +0530

    halfway done

--- Web Projects/StepProgressBar/App.js
+++ Web Projects/StepProgressBar/App.js
@@ -4,17 +4,13 @@ let totalWidth = form.offsetWidth;
 let width = bar.offsetWidth;
 
 function prevEvent(){
-    console.log(width);
     if(width > 1){
-        width -= (20/100)*(totalWidth);
         bar.style.width = width;
     }
 }
 
 function nextEvent(){
-    console.log(width);
     if(width < totalWidth){
-        width += (20/100)*(totalWidth);
         bar.style.width = width;
     }
 }
