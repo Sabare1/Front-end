7c85afcc85b247b35bf63a8253ee9a4f693fcd85
Sabare1 <sabaregirivasan@gmail.com>
2025-07-10 13:06:54 +0530

    made load work proper

--- Web Projects/StepProgressBar/App.js
+++ Web Projects/StepProgressBar/App.js
@@ -5,63 +5,61 @@ let step2 = document.querySelector(".step2");
 let step3 = document.querySelector(".step3");
 let step4 = document.querySelector(".step4");
 let step5 = document.querySelector(".step5");
-let bar1 = document.querySelector("#bar1");
-let bar2 = document.querySelector("#bar2");
-let bar3 = document.querySelector("#bar3");
-let bar4 = document.querySelector("#bar4");
-let bar5 = document.querySelector("#bar5");
+let bar = document.querySelector(".bar");
+
 let step = 0;
 
 
 
 function prev(){
-    if(step < 0){
+    let percent = (step/(5))*100;
+    if(step <= 0){
         step = 0;
     }
-    else if(step == 1){
+    else{
+        prevLoad(bar, percent);
         step--;
-        bar1.style.width = 0+"%";
-    }
-    else if(step == 2){
-        step--;
-        bar2.style.width = 0+"%";
-    }
-    else if(step == 3){
-        step--;
-        bar3.style.width = 0+"%";
-    }
-    else if(step == 4){
-        step--;
-        bar4.style.width = 0+"%";
-    }
-    else if(step == 5){
-        step--;
-        bar5.style.width = 0+"%";
     }
 }
 
 function next(){
-    if(step > 5){
+    console.log(step);
+    let percent = (step/(5))*100;
+    if(step >= 5){
         step = 5;
     }
-    else if(step == 0){
+    else{
+        nextLoad(bar, percent);
         step++;
-        bar1.style.width = 100 + "%";
     }
-    else if(step == 1){
-        step++;
-        bar2.style.width = 100 + "%";
+}
+
+function prevLoad(elem, percent){
+    let minPercent = percent - 20;
+    let temp = setInterval(frame, 10);
+
+    function frame(){
+        if(percent <= minPercent){
+            clearInterval(temp);
+        }
+        else{
+            percent--;
+            elem.style.width = percent+"%";
+        }
     }
-    else if(step == 2){
-        step++;
-        bar3.style.width = 100 + "%";
-    }
-    else if(step == 3){
-        step++;
-        bar4.style.width = 100 + "%";
-    }
-    else if(step == 4){
-        step++;
-        bar5.style.width = 100 + "%";
+}
+
+function nextLoad(elem, percent){
+    let maxPercent = percent+20;
+    let temp = setInterval(frame, 10);
+
+    function frame(){
+        if(percent >= maxPercent){
+            clearInterval(temp);
+        }
+        else{
+            percent++;
+            elem.style.width = percent+"%";
+        }
     }
 }
