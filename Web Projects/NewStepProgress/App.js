7c89ccffa9192f28672a90ff983f016cfbed16df
Sabare1 <sabaregirivasan@gmail.com>
2025-07-11 12:20:33 +0530

    did html and some css

---
+++ Web Projects/NewStepProgress/App.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let steps = document.querySelector(".step-container");
+    let forms = document.querySelector(".form-container");
+    
+});
