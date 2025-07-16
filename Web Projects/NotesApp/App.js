b93fc4c22eca8ed7b0541b2e15c012af8763f0e2
Sabare1 <sabaregirivasan@gmail.com>
2025-07-16 11:16:09 +0530

    fixed the script bugs

--- Web Projects/NotesApp/App.js
+++ Web Projects/NotesApp/App.js
@@ -1,28 +1,36 @@
-
 let saveNotes = () =>{
 
-    let titles = document.querySelectorAll(".title");
-    let bodys = document.querySelectorAll(".body");
+    let titleItems = document.querySelectorAll(".title");
+    let bodyItems = document.querySelectorAll(".body");
     let data = [];
-    titles.forEach((titles, index) => {
-        let head = titles.value;
-        let content = bodys[index].value;
+    titleItems.forEach((item, index) => {
+        let head = item.value;
+        let content = bodyItems[index].value;
         if(content.trim() !== ""){
             data.push({head, content});
         }
     })
+
+    let titlesData = data.map((item) => item.head);
+    let bodysData = data.map((item) => item.content);
+    localStorage.setItem('titles', JSON.stringify(titlesData));
+    localStorage.setItem('bodys', JSON.stringify(bodysData));
 }
 
-let addNote = () => {
+let addNote = (title="", content="") => {
+    if(title instanceof Event){
+        title = "";
+    }
     let newNote = document.createElement("div");
+    console.log(newNote);
     newNote.classList.add("note");
     newNote.innerHTML = `
     <div class="icons-container">
           <input type="image" class="save" src="save.png" />
           <input type="image" class="bin" src="bin.png" />
         </div>
-        <textarea class="title" placeholder="Enter title"></textarea><br />
-        <textarea class="body" placeholder="Enter your thoughts"></textarea>
+        <textarea class="title" name="title" placeholder="Enter title">${title}</textarea>
+        <textarea class="body" name="body" placeholder="Enter your thoughts">${content}</textarea>
     `;
 
     function saveContent(){
@@ -46,3 +54,15 @@ let addNote = () => {
 
 let addBtn = document.querySelector(".add");
 addBtn.addEventListener("click", addNote);
+
+function loadNotes(){
+    const titleData = localStorage.getItem("titles");
+    const bodyData = localStorage.getItem("bodys");
+    let titleLen = (titleData !== null) ? titleData.length : 0;
+    let bodyLen = (bodyData !== null) ? bodyData.length : 0;
+    for(let i=0; i< titleLen && bodyLen; i++){
+        addNote(titleData[i], bodyData[i]);
+    }
+}
+
+loadNotes();
